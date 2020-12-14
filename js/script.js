aca7ef607be23d6458777360a9608612b7b14929
Justyna Szewczyk <justyna0829@gmail.com>
2020-12-14 11:47:25 +0100

    Add function of displaying the article after clicking

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 'use strict';
 
 const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
 
@@ -25,11 +26,19 @@ const titleClickHandler = function(event){
     activeArticle.classList.remove('active');
     }
 
-    /* get 'href' attribute from the clicked link */
+    /* [DONE] get 'href' attribute from the clicked link */
 
-    /* find the correct article using the selector (value of 'href' attribute) */
+    const articleSelector = clickedElement.getAttribute ("href");
+    console.log(articleSelector);
 
-    /* add class 'active' to the correct article */
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
+
+    /* [DONE]  add class 'active' to the correct article */
+
+    targetArticle.classList.add('active');
 }
   
 const links = document.querySelectorAll('.titles a');
